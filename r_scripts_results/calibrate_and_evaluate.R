916202a9869cb8d73c440bef8c0946c22698c535
keisersoze <filippomaganza@gmail.com>
2021-12-05 11:39:20 +0100

    npc for location

--- r_scripts_results/calibrate_and_evaluate.R
+++ r_scripts_results/calibrate_and_evaluate.R
@@ -14,45 +14,49 @@ calib.seed = 45
 calib.m = 100
 calib.n = 5
 
-calib.nsim = 10000
+calib.nsim = 50000
+
 # npc tippet
 # calib.limits_seq = inverse(seq(2, 9.5, 0.001))
-# calib.limits_seq = inverse(seq(2, 6.2, 0.001))
+calib.limits_seq = inverse(seq(2, 5.8, 0.001))
+
 # npc fisher
 # calib.limits_seq = seq(-2, -5, -0.001)
 # calib.limits_seq = seq(-2, -11, -0.001)
 # calib.limits_seq = seq(-2, -13.2, -0.0005)
+
 # npc liptak
 # calib.limits_seq = seq(1.02, 1.002, -0.0001)
+
 # lepage
 # calib.limits_seq = seq(2, 11, 0.001)
+
 # cucconi
 # calib.limits_seq = seq(2, 5.7, 0.001)
+
 # van_de_warden
 # calib.limits_seq = seq(4, 5.6, 0.001)
-# calib.is_upper_limit = TRUE
+
 # mann_whitney
 # calib.limits_seq = seq(400,430,1)
-calib.is_upper_limit = TRUE
+calib.is_upper_limit = FALSE
 
-calib.ARL0.target = 500
+calib.ARL0.target = 370
 
-calib.monitor_stat = "simple_statistic"
-calib.monitor_stat.params = list(statistic="mann_whitney")
-# calib.monitor_stat.params = list(
-#   "statistics"= list(
-#     "mann_whitney",
-#     "sum_of_sings"
-#   ),
-#   "permutation_distributions"=list(
-#     compute_permutation_distribution("mann_whitney", calib.m, calib.n, 10000),
-#     compute_permutation_distribution("sum_of_sings", calib.m, calib.n, 10000)
-#   ),
-#   "tails"=list(
-#     "left",
-#     "right"
-#   )
-# )
+calib.monitor_stat = "npc"
+# calib.monitor_stat.params = list(statistic="mann_whitney")
+calib.monitor_stat.params = list(
+  "statistics"= list(
+    "centered_wilcoxon_rank_sum"
+  ),
+  "permutation_distributions"=list(
+    compute_permutation_distribution("centered_wilcoxon_rank_sum", calib.m, calib.n, 10000)
+  ),
+  "tails"=list(
+    "two_sided"
+  ),
+  "combining_function"="tippet"
+)
 
 calib.dist = "norm"
 calib.dist.params = list("mean" = 0 , "sd" =  1)
@@ -62,7 +66,7 @@ calib.cap = 25000
 calib.eval.dist = "norm"
 calib.eval.dist.params = list("mean"= 0, "sd"= 1)
 calib.eval.nsim = calib.nsim
-calib.eval.location_shifts = seq(0, -1, -0.1)
+calib.eval.location_shifts = seq(0, 1, 0.1)
 calib.eval.scale_multipliers = c(1)
 calib.eval.metrics = c("mean", "sd")
 
